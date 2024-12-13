af4f705556dd394805092c6121b8d1bd13e8bb98
Abner Augusto Souza <abner.souza@bugaboostudio.com>
2024-12-13 15:55:49 -0300

    more efficient normal calculation from box

--- qbRaytracing/box.h
+++ qbRaytracing/box.h
@@ -5,80 +5,72 @@
 #include "vec3.h"
 #include "material.h"
 #include "interval.h"
+#include <algorithm>
+#include <cmath>
 
 class box : public hittable {
 public:
+    // Public parameters
+    point3 min_corner;
+    point3 v_max;
+    mat material;
+
+    // Constructor
     box(const point3& v_min, const point3& v_max, const mat& material)
         : min_corner(v_min), v_max(v_max), material(material) {
     }
 
-    void set_min_corner(const point3& new_min_corner) {
-        min_corner = new_min_corner;
-    }
-
-    void set_max_corner(const point3& new_max_corner) {
-        v_max = new_max_corner;
-    }
-
-    void set_material(const mat& new_material) {
-        material = new_material;
-    }
-
+    // Hit function
     bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
+        const vec3& origin = r.origin();
+        const vec3& direction = r.direction();
+
         double t_min = ray_t.min;
         double t_max = ray_t.max;
+        int hit_axis = -1;
 
-        // Iterate over x, y, z axes
         for (int i = 0; i < 3; ++i) {
-            double inv_d = 1.0 / r.direction()[i];
-            double t0 = (min_corner[i] - r.origin()[i]) * inv_d;
-            double t1 = (v_max[i] - r.origin()[i]) * inv_d;
-
-            if (inv_d < 0.0) {
-                std::swap(t0, t1);
+            if (std::fabs(direction[i]) < 1e-12) {
+                if (origin[i] < min_corner[i] || origin[i] > v_max[i]) {
+                    return false;
+                }
             }
+            else {
+                double inv_d = 1.0 / direction[i];
+                double t0 = (min_corner[i] - origin[i]) * inv_d;
+                double t1 = (v_max[i] - origin[i]) * inv_d;
+                if (t0 > t1) std::swap(t0, t1);
 
-            t_min = std::fmax(t0, t_min);
-            t_max = std::fmin(t1, t_max);
+                if (t0 > t_min) {
+                    t_min = t0;
+                    hit_axis = i;
+                }
+                t_max = (t1 < t_max) ? t1 : t_max;
 
-            if (t_max <= t_min) {
-                return false;
+                if (t_max <= t_min) {
+                    return false;
+                }
             }
         }
 
         rec.t = t_min;
         rec.p = r.at(t_min);
+        rec.material = &material;
 
-        // Determine which face was hit
-        vec3 outward_normal;
-        for (int i = 0; i < 3; ++i) {
-            if (std::fabs(rec.p[i] - min_corner[i]) < 1e-4) {
-                outward_normal[i] = -1.0;
-            }
-            else if (std::fabs(rec.p[i] - v_max[i]) < 1e-4) {
-                outward_normal[i] = 1.0;
-            }
-            else {
-                outward_normal[i] = 0.0;
-            }
+        vec3 outward_normal(0, 0, 0);
+        if (hit_axis >= 0) {
+            double mid_point = 0.5 * (min_corner[hit_axis] + v_max[hit_axis]);
+            outward_normal[hit_axis] = (rec.p[hit_axis] < mid_point) ? -1.0 : 1.0;
         }
 
         rec.set_face_normal(r, outward_normal);
-        rec.material = &material;
 
-        // Ignore back faces
         if (!rec.front_face) {
             return false;
         }
 
         return true;
     }
-
-
-private:
-    point3 min_corner;
-    point3 v_max;
-    mat material;
 };
 
 #endif
