b5f25127f88cec8c1595ef81d35bd9bd687c9c84
Abner Augusto Souza <abner.souza@bugaboostudio.com>
2025-01-03 09:48:28 -0300

    box now accepts transformations matrices

--- RayTracer/box.h
+++ RayTracer/box.h
@@ -3,6 +3,7 @@
 
 #include "hittable.h"
 #include "vec3.h"
+#include "matrix4x4.h"
 #include "material.h"
 #include "interval.h"
 #include "primitive.h"
@@ -27,6 +28,13 @@ public:
         vec3 half_width(width / 2.0, width / 2.0, width / 2.0);
         min_corner = center - half_width;
         v_max = center + half_width;
+    }
+
+    // Constructor using min corner and dimensions
+    box(const point3& v_min, double length, double width, double height, const mat& material)
+        : min_corner(v_min),
+        v_max(v_min + point3(length, width, height)),
+        material(material) {
     }
 
     // Hit function
@@ -101,6 +109,50 @@ public:
         return 'g';
     }
 
+    void apply_transformation(const Matrix4x4& transform) {
+        // Get all vertices of the box
+        std::vector<point3> vertices = get_vertices();
+
+        // Transform each vertex
+        for (point3& vertex : vertices) {
+            // Convert point3 to vec4 (homogeneous coordinates)
+            vec4 homogenous_vertex(vertex.x(), vertex.y(), vertex.z(), 1.0);
+
+            // Apply the transformation matrix
+            vec4 transformed_vertex = transform * homogenous_vertex;
+
+            // Convert back to point3 (Cartesian coordinates)
+            vertex = transformed_vertex.to_vec3();
+        }
+
+        update_bounds(vertices);
+    }
+
+    // Get all vertices of the box
+    std::vector<point3> get_vertices() const {
+        return {
+            min_corner,
+            point3(v_max.x(), min_corner.y(), min_corner.z()),
+            point3(v_max.x(), v_max.y(), min_corner.z()),
+            point3(min_corner.x(), v_max.y(), min_corner.z()),
+            point3(min_corner.x(), min_corner.y(), v_max.z()),
+            point3(v_max.x(), min_corner.y(), v_max.z()),
+            point3(v_max.x(), v_max.y(), v_max.z()),
+            point3(min_corner.x(), v_max.y(), v_max.z()),
+        };
+    }
+
+private:
+    // Update bounds based on a list of vertices
+    void update_bounds(const std::vector<point3>& vertices) {
+        min_corner = vertices[0];
+        v_max = vertices[0];
+
+        for (const auto& vertex : vertices) {
+            min_corner = min(min_corner, vertex); // Update minimum bounds
+            v_max = max(v_max, vertex);          // Update maximum bounds
+        }
+    }
 };
 
 #endif
