8e1bf44bf5179d826638041ff629d413b0dae154
Abner Augusto Souza <abner.souza@bugaboostudio.com>
2025-01-04 18:49:47 -0300

    update raytracer.h

--- RayTracer/raytracer.h
+++ RayTracer/raytracer.h
@@ -31,11 +31,11 @@ const double pi = 3.1415926535897932385;
 // Utility Functions
 
 inline double degrees_to_radians(double degrees) {
-    return degrees * M_PI / 180.0;
+    return degrees * pi / 180.0;
 }
 
 inline double radians_to_degrees(double radians) {
-    return radians * 180.0 / M_PI;
+    return radians * 180.0 / pi;
 }
 
 inline double random_double(double min, double max) {
