1e646ff4aa5b505a3f0e457e9df410f2cf3a425a
Abner Augusto Souza <abner.souza@bugaboostudio.com>
2025-01-16 19:30:11 -0300

    separate hit_record from hittable

---
+++ RayTracer/hit_record.h
@@ -0,0 +1,38 @@
+// hit_record.h
+#ifndef HIT_RECORD_H
+#define HIT_RECORD_H
+
+#include "vec3.h"
+#include "ray.h"
+
+class mat; // Forward declaration for material
+
+class hit_record {
+public:
+    point3 p = point3(0, 0, 0); // Collision point
+    vec3 normal = vec3(0, 0, 0); // Normal at collision point
+    double t = 0.0; // Ray parameter
+    bool front_face = true; // Indicates if the collision is external or internal
+    const mat* material = nullptr; // Pointer to the material
+    double u = 0.0; // Horizontal UV coordinate
+    double v = 0.0; // Vertical UV coordinate
+
+    hit_record() = default;
+
+    void reset() {
+        p = point3(0, 0, 0);
+        normal = vec3(0, 0, 0);
+        t = 0.0;
+        front_face = true;
+        material = nullptr;
+        u = 0.0;
+        v = 0.0;
+    }
+
+    inline void set_face_normal(const ray& r, const vec3& outward_normal) {
+        front_face = dot(r.direction(), outward_normal) < 0;
+        normal = front_face ? outward_normal : -outward_normal;
+    }
+};
+
+#endif
--- RayTracer/hittable.h
+++ RayTracer/hittable.h
@@ -5,34 +5,24 @@
 #include "vec3.h"
 #include "material.h"
 #include "interval.h"
+#include "matrix4x4.h"
+#include "hit_record.h"
 
-class hit_record {
-public:
-    point3 p; // Ponto da colisão
-    vec3 normal; // Normal no ponto de colisão
-    double t = 0.0; // Valor da equação da reta
-    bool front_face = true; // Para indicar se a colisão foi externo ou interno
-    const mat* material = nullptr; // Ponteiro para o material do objeto colidido
-    double u = 0.0; // Coordenada UV horizontal
-    double v = 0.0; // Coordenada UV vertical
-
-    hit_record() = default;
-
-    inline void set_face_normal(const ray& r, const vec3& outward_normal) {
-        front_face = dot(r.direction(), outward_normal) < 0;
-        normal = front_face ? outward_normal : -outward_normal;
-    }
-};
+// Forward declaration to avoid circular dependency
+class BoundingBox;
 
 class hittable {
 public:
     virtual ~hittable() = default;
-
     virtual bool hit(const ray& r, interval ray_t, hit_record& rec) const = 0;
 
     // Transform the object using a 4x4 matrix
-    virtual void transform(const Matrix4x4& transform) = 0;
+    virtual void transform(const Matrix4x4& transform) {
+        throw std::runtime_error("Transform not supported for this object.");
+    }
 
+    // Retrieve the bounding box of the object
+    virtual BoundingBox bounding_box() const = 0;
 };
 
 #endif
